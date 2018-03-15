175681446552993b736578294866e7150948219f
ajduv <ajduvanenko@gmail.com>
2018-03-15 12:06:29 -0400

    Learned about templating and how PUG works

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'Wes', age: 100, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello', {
+    name: 'wes',
+    dog: req.query.dog
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
