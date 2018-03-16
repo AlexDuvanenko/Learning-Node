c619f333c30a3f8858c074bdea526a5a8ed5b27e
ajduv <ajduvanenko@gmail.com>
2018-03-16 13:43:31 -0400

    Queried the db and created the stores view files to show the list of all the stores in the db.

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-    console.log(req.name);
     res.render('index');
 };
 
@@ -15,3 +14,10 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    // 1. Query the database for a list of all stores
+    const stores = await Store.find();
+    console.log(stores);
+    res.render('stores', { title: 'Stores', stores });
+}
