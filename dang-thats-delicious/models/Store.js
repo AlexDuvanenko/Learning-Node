b75f69b6f3d70c395fcb4858e1cd72f4a555b90d
ajduv <ajduvanenko@gmail.com>
2018-03-19 13:31:40 -0400

    Created functionality that gave each store it's own page, and created unique slugs for stores that have the same name

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -35,12 +35,19 @@ const storeSchema = new mongoose.Schema({
     photo: String
 });
 
-storeSchema.pre('save', function(next) { // can't use arrow function here because we need 'this'
+storeSchema.pre('save', async function(next) { // can't use arrow function here because we need 'this'
     if (!this.isModified('name')) {
         next(); // skip it
         return; // stop this function from running
     }
     this.slug = slug(this.name);
+    // find other stores that have a slug of wes, wes-1, wes-2
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx }); // this.constructor.find = Store
+    if (storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+
     next();
     // TODO make more resiliant so slugs are unique
 });
