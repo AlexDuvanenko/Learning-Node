265a5f8c8a68db804055e233a9985cc716e52227
ajduv <ajduvanenko@gmail.com>
2018-03-23 15:33:44 -0400

    Created top stores page using advanced aggregation

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -158,3 +158,9 @@ exports.getHearts = async (req, res) => {
     
     res.render('stores', { title: 'Hearted Stores', stores } );
 };
+
+exports.getTopStores = async (req, res) => {
+    const stores = await Store.getTopStores();
+    //res.json(stores);
+    res.render('topStores', { stores, title: '★ Top Stores!'});
+}
